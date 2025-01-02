0125d89dd2f074fee37947694c7f8bdbed6ae1f3
GG-Angel <122509517+GG-Angel@users.noreply.github.com>
2025-01-01 21:56:47 -0600

    Added colors

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      txt: {
+        light: "#0C0C0D",
+        dark: "#FFFFFF",
+        alt: "#858586"
+      },
+      bg: {
+        light: "#F5F5F7",
+        dark: "#151419"
+      },
+      card: {
+        light: "#FFFFFF",
+        dark: "#0C0C0D"
+      },
+      diff: {
+        expertplus: "#945FD3",
+        expert: "#BF2A42",
+        hard: "#FF6347",
+        normal: "#59B0F4",
+        easy: "#3CB371"
+      },
+      green: {
+        DEFAULT: "#3CB371"
+      },
+      blue: {
+        DEFAULT: "#59B0F4"
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
