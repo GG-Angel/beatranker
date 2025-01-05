1bfdeb910710a6372fa4160561e365014ec8a30c
GG-Angel <122509517+GG-Angel@users.noreply.github.com>
2025-01-05 00:40:57 -0500

    New colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,11 @@ export default {
         },
         card: {
           light: "#FFFFFF",
-          dark: "#0C0C0D"
+          dark: "#0C0C0D",
+          alt: {
+            light: "#E4E4E4",
+            dark: "#242436"
+          }
         },
         diff: {
           expertplus: "#945FD3",
