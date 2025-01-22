23da216455cbb53cdda9b71131eafd4721e19b14
GG-Angel <122509517+GG-Angel@users.noreply.github.com>
2025-01-21 19:54:57 -0500

    Extracted colors to separate file

---
+++ constants/colors.ts
@@ -0,0 +1,42 @@
+export default {
+  tx: {
+    light: "#0C0C0D",
+    dark: "#FFFFFF",
+    alt: "#858586"
+  },
+  bg: {
+    light: "#F5F5F7",
+    dark: "#151419"
+  },
+  card: {
+    light: "#FFFFFF",
+    dark: "#0C0C0D",
+    alt: {
+      light: "#E4E4E4",
+      dark: "#242436"
+    }
+  },
+  active: {
+    light: "#646cff",
+    dark: "#404078"
+  },
+  diff: {
+    expertplus: "#945FD3",
+    expert: "#BF2A42",
+    hard: "#FF6347",
+    normal: "#59B0F4",
+    easy: "#3CB371"
+  },
+  red: {
+    light: "#BF2A42", 
+    dark: "#F87171"
+  },
+  green: {
+    light: "#168E4C",
+    dark: "#48CB83",
+  },
+  blue: {
+    light: "#2991E1",
+    dark: "#2592E7"
+  }
+}
--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 import Icons from "./icons"
 import Images from "./images"
+import Colors from "./colors"
 
-export { Icons, Images };
+export { Icons, Images, Colors };
